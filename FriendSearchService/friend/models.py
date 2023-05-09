b51eb68d7a6186e53ca8d574127f70c0d3a1c623
allozo <allozo@yandex.ru>
2023-05-09 15:20:15 +0300

    Добавил models для пользователей

---
+++ FriendSearchService/friend/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    id = models.PositiveIntegerField(primary_key=True)
+    name = models.CharField(max_length=20)
+    is_deleted = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Friendship(models.Model):
+    user_id_1 = models.ManyToManyField(User, related_name='+')
+    user_id_2 = models.ManyToManyField(User, related_name='+')
+
+
+class ApplicationToFriends(models.Model):
+    user_id_1 = models.ManyToManyField(User, related_name='+')
+    user_id_2 = models.ManyToManyField(User, related_name='+')
