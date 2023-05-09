822fb120d2da9f23e2786d483a29d16ab1ba9717
allozo <allozo@yandex.ru>
2023-05-09 19:24:45 +0300

    FEAT-#3: Добавил ограничения для доступа к API
    Только авторизованные пользователи смогут пользоваться API.

--- FriendSearchService/friend/views.py
+++ FriendSearchService/friend/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics, viewsets, mixins
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet
+from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import render
 
 from friend.models import User, Friendship, ApplicationToFriends
@@ -10,18 +11,14 @@ from friend.serializers import UserSerializer
 
 
 class UserViewSet(
-        mixins.CreateModelMixin,
         mixins.RetrieveModelMixin,
         mixins.ListModelMixin,
+        mixins.CreateModelMixin,
         GenericViewSet
     ):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
-
-# class UsersAPIList(generics.ListAPIView):
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated, )
 
 
 # class UserAPIView(generics.RetrieveAPIView):
